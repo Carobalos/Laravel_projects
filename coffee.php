e9c90e4018acde924951d4af5524cc652902d2da
Carobalos <carobalos@gmail.com>
2015-07-16 13:09:27 +0300

    Commands

---
+++ coffee.php
@@ -0,0 +1,21 @@
+<?php
+function caffeineBuzz (int $y) {
+if
+  ($y%3==0)
+  return "Java"
+if
+  ($y%3==0)
+else if 
+  ($y%4==0)
+return "coffee"
+if 
+  ($y%2==0)
+else if
+   ($y%3==0)
+else if
+   ($y%4==0)
+return "coffeeScript"
+else 
+return "mocha_missing"
+} 
+?>
